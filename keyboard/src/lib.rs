c9e23973071aeacaaf35800c70daf7ff06240c63
Yaw Boakye <wheresyaw@gmail.com>
2023-05-15 11:50:17 +0100

    preparing to handle keyboard events
    mainly key presses and releases

--- keyboard/src/lib.rs
+++ keyboard/src/lib.rs
@@ -3,12 +3,19 @@
 #![feature(trait_alias)]
 
 use pc_keyboard::{
-    layouts, HandleControl, Keyboard as PCKeyboard, KeyboardLayout as PCKeyboardLayout,
-    ScancodeSet as PCScancodeSet, ScancodeSet1,
+    layouts, HandleControl, KeyEvent as PCKeyEvent, Keyboard as PCKeyboard,
+    KeyboardLayout as PCKeyboardLayout, ScancodeSet as PCScancodeSet,
+    ScancodeSet1 as PCScancodeSet1,
 };
 
+pub type KeyEvent = PCKeyEvent;
+
+// aliases for the purpose of export
 pub trait Layout = PCKeyboardLayout;
 pub trait ScancodeSet = PCScancodeSet;
+
+pub type UsLayout = layouts::Us104Key;
+pub type ScancodeSet1 = PCScancodeSet1;
 
 pub struct Keyboard<L: Layout, S: ScancodeSet> {
     keyboard: PCKeyboard<L, S>,
@@ -26,7 +33,15 @@ impl<L: Layout, S: ScancodeSet> Keyboard<L, S> {
         Keyboard { keyboard }
     }
 
-    pub fn us_keyboard() -> Keyboard<layouts::Us104Key, ScancodeSet1> {
+    pub fn us_scancode_set_one_keyboard() -> Keyboard<UsLayout, ScancodeSet1> {
         Keyboard::new(layouts::Us104Key, ScancodeSet1::new())
+    }
+
+    pub fn process_key_event<F, R>(&self, f: F) -> R
+    where
+        F: FnOnce(Option<KeyEvent>) -> R,
+    {
+        let key_event = todo!();
+        f(key_event)
     }
 }
