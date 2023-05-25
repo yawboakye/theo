073a10d2579f418c19322be5a1a112caae173b2b
Yaw Boakye <wheresyaw@gmail.com>
2023-05-25 11:36:49 +0100

    process decoded key

--- keyboard/src/lib.rs
+++ keyboard/src/lib.rs
@@ -3,22 +3,23 @@
 #![feature(trait_alias)]
 
 use pc_keyboard::{
-    layouts, HandleControl, KeyEvent as PCKeyEvent, Keyboard as PCKeyboard,
+    layouts, DecodedKey as PCDecodedKey, HandleControl, Keyboard as PCKeyboard,
     KeyboardLayout as PCKeyboardLayout, ScancodeSet as PCScancodeSet,
     ScancodeSet1 as PCScancodeSet1,
 };
-
-pub type KeyEvent = PCKeyEvent;
+use x86_64::instructions::port::Port;
 
 // aliases for the purpose of export
 pub trait Layout = PCKeyboardLayout;
 pub trait ScancodeSet = PCScancodeSet;
 
+pub type DecodedKey = PCDecodedKey;
 pub type UsLayout = layouts::Us104Key;
 pub type ScancodeSet1 = PCScancodeSet1;
 
 pub struct Keyboard<L: Layout, S: ScancodeSet> {
     keyboard: PCKeyboard<L, S>,
+    port: Port<u8>,
 }
 
 impl<L: Layout, S: ScancodeSet> Keyboard<L, S> {
@@ -30,18 +31,25 @@ impl<L: Layout, S: ScancodeSet> Keyboard<L, S> {
             HandleControl::Ignore,
         );
 
-        Keyboard { keyboard }
+        Keyboard {
+            port: Port::<u8>::new(0x60),
+            keyboard,
+        }
     }
 
     pub fn us_scancode_set_one_keyboard() -> Keyboard<UsLayout, ScancodeSet1> {
         Keyboard::new(layouts::Us104Key, ScancodeSet1::new())
     }
 
-    pub fn process_key_event<F, R>(&self, f: F) -> R
+    pub fn process_decoded_key<F, R>(&mut self, f: F)
     where
-        F: FnOnce(Option<KeyEvent>) -> R,
+        F: FnOnce(DecodedKey) -> R,
     {
-        let key_event = todo!();
-        f(key_event)
+        let scancode = unsafe { self.port.read() };
+        if let Ok(Some(key_event)) = self.keyboard.add_byte(scancode) {
+            if let Some(decoded_key) = self.keyboard.process_keyevent(key_event) {
+                f(decoded_key);
+            }
+        }
     }
 }
